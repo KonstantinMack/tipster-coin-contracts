d9dd933fb7902b19b3d8d2e7c418dfccccd8e263
Konstantin Mack <mack.konstantin@googlemail.com>
2023-11-13 10:42:22 +0800

    Make factory ownable and restrict reusing same ticker

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,28 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token is ERC20 {
-    address private _owner = 0xefd267e5314297343098B5D597171a4467f62849;
     // TODO: add community address
     // address private _community = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-    constructor(string memory _name, string memory _ticker, uint256 _supply, address _user) ERC20(_name, _ticker) {
+    constructor(string memory _name, string memory _ticker, uint256 _supply, address _user, address _maintainer) ERC20(_name, _ticker) {
       _mint(_user, _supply * 80 / 100);
-      _mint(_owner, _supply * 10 / 100);
+      _mint(_maintainer, _supply * 10 / 100);
       // _mint(_community, _supply * 10 / 100);
     }
 }
 
-contract Factory {
+contract Factory is Ownable {
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
     address[] public tokens;
     uint256 public tokenCount;
+    mapping(string => bool) private usedTickers;
     event TokenDeployed(address tokenAddress);
 
     function deployToken(string calldata _name, string calldata _ticker, uint256 _supply) public returns (address) {
-        Token token = new Token(_name, _ticker, _supply, msg.sender);
+        string memory key = string(abi.encodePacked(msg.sender, _ticker));
+        require(!usedTickers[key], "Ticker already used by this address");
+        Token token = new Token(_name, _ticker, _supply, msg.sender, owner());
         tokens.push(address(token));
         tokenCount += 1;
+        usedTickers[key] = true;
         emit TokenDeployed(address(token));
         return address(token);
     }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,23 @@
 const hre = require("hardhat");
 
-async function main() {
-  const deployedContract = await hre.ethers.deployContract("Factory");
-  await deployedContract.waitForDeployment();
-  console.log(
-    `Contract deployed to https://explorer.toronto.sx.technology/address/${deployedContract.target}`
-  );
-}
-
 // async function main() {
-//   const [deployer] = await hre.ethers.getSigners();
-
-//   console.log("Deploying contracts with the account:", deployer.address);
-
-//   const Token = await hre.ethers.getContractFactory("Token");
-//   const token = await Token.deploy(
-//     "TestToken4",
-//     "TT4",
-//     1_000_000 * 10 ** 18
+//   const deployedContract = await hre.ethers.deployContract("Factory");
+//   await deployedContract.waitForDeployment();
+//   console.log(
+//     `Contract deployed to https://explorer.toronto.sx.technology/address/${deployedContract.target}`
 //   );
-//   await token.waitForDeployment();
-//   const address = await token.getAddress();
-//   console.log(`Token address: ${address}`);
 // }
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Factory = await hre.ethers.getContractFactory("Factory");
+  const deployedContract = await Factory.deploy(deployer.address);
+  await deployedContract.waitForDeployment();
+  const address = await deployedContract.getAddress();
+  console.log(`Contract deployed to adddress: ${address}`);
+}
 
 main().catch((error) => {
   console.error(error);
